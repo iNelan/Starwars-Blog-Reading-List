0f2e26a132e1cfc737a0e12af2c5facbf7dd775b
iNelan <clpayala@gmail.com>
2022-06-11 10:41:06 +0000

    version2

---
+++ src/js/component/VehicleCard.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const VehicleCard = () => {
+  return (
+    <>
+      <div className="card me-3" style={{ width: "18rem" }}>
+        <img
+          src="https://img.redbull.com/images/q_auto,f_auto/redbullcom/2015/12/27/1331767413572_2/star-wars-spaceships-x-wing.jpg"
+          alt="placehold.it"
+          className="card-img-top"
+        />
+        <div className="card-body">
+          <h5 className="card-title">Rey</h5>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">Max. Speed:</li>
+            <li className="list-group-item">color:</li>
+            <li className="list-group-item">Flight:</li>
+            <li className="list-group-item"></li>
+          </ul>
+          <Link to="/detailCharacter"> 
+          <span href="#" className="btn btn-outline-primary">
+            Learn More!
+          </span></Link>
+          <button type="button" className="btn btn-outline-warning float-end">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-heart"
+              viewBox="0 0 16 16"
+              color="orange"
+            >
+              <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/js/views/home.jsx
+++ src/js/views/home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "../../styles/home.css";
 import { CharacterCard } from "../component/CharacterCard.jsx";
 import { PlanetCard } from "../component/PlanetCard.jsx";
+import { VehicleCard } from "../component/VehicleCard.jsx";
+
 
 
 
@@ -10,22 +12,32 @@ export const Home = () => {
   <div className="container">
 
 	  <h1>Characters</h1>
-    <div class="d-flex overflow-auto mt-5 Characters">
-      <div class="col-3"><CharacterCard /></div>
-      <div class="col-3"><CharacterCard /></div>
-      <div class="col-3"><CharacterCard /></div>
-      <div class="col-3"><CharacterCard /></div>
-      <div class="col-3"><CharacterCard /></div>
+    <div className="d-flex overflow-auto mt-5 Characters">
+      <div className="col-3"><CharacterCard /></div>
+      <div className="col-3"><CharacterCard /></div>
+      <div className="col-3"><CharacterCard /></div>
+      <div className="col-3"><CharacterCard /></div>
+      <div className="col-3"><CharacterCard /></div>
     </div>
 
 
     <h1 className="mt-5">Planets</h1>
-    <div className="d-flex overflow-auto Planetcard mt-2">
-      <div class="col-3"><PlanetCard /></div>
-      <div class="col-3"><PlanetCard /></div>
-      <div class="col-3"><PlanetCard /></div>
-      <div class="col-3"><PlanetCard /></div>
-      <div class="col-3"><PlanetCard /></div>
+    <div className="d-flex overflow-auto Planetcard mt-5">
+      <div className="col-3"><PlanetCard /></div>
+      <div className="col-3"><PlanetCard /></div>
+      <div className="col-3"><PlanetCard /></div>
+      <div className="col-3"><PlanetCard /></div>
+      <div className="col-3"><PlanetCard /></div>
+    </div>
+
+
+    <h1 className="mt-5">Vehicles</h1>
+    <div className="d-flex overflow-auto Planetcard mt-5">
+      <div className="col-3"><VehicleCard /></div>
+      <div className="col-3"><VehicleCard /></div>
+      <div className="col-3"><VehicleCard /></div>
+      <div className="col-3"><VehicleCard /></div>
+      <div className="col-3"><VehicleCard /></div>
     </div>
 
   </div>
